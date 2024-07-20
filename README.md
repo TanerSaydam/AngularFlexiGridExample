9e0583f16063ec08f6c542db028a358a0914d384
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2024-07-20 23:46:29 +0300

    .

---
+++ README.md
@@ -0,0 +1,9 @@
+# Angular Flexi Grid Example
+
+Youtube videosu:
+https://youtu.be/7gq7yXwA4Yg
+
+Dökümantasyon:
+https://flexi-ui.ecnorow.com/flexi-grid
+
+Repoya yıldız vererek destek vermeyi unutmayın :)
---
+++ Server.WebAPI/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.WebAPI.Models;
+
+namespace Server.WebAPI.Context;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    { }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().Property(p => p.FirstName).HasColumnType("varchar(50)");
+        modelBuilder.Entity<User>().Property(p => p.LastName).HasColumnType("varchar(50)");
+        modelBuilder.Entity<User>().Property(p => p.Salary).HasColumnType("money");
+    }
+}
